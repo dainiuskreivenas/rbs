72e0dbdbde0510dec85fe2e9f7b1dba1c4bd4b4c
Dainius <dainius.kreivenas@gmail.com>
2019-11-18 19:42:33 +0000

    Fixing Tests

--- tests/Assert_Multiply_Numerics.py
+++ tests/Assert_Multiply_Numerics.py
@@ -14,7 +14,7 @@ sim.setup(timestep=1.0,min_delay=1.0,max_delay=1.0, debug=0)
 simName = "nest"
 neal = NealCoverFunctions(simName, sim)
 fsa = FSAHelperFunctions(simName, sim, neal)
-narc = NeuralCognitiveArchitectureBuilder(sim, simName, fsa, neal).build()
+narc = NeuralCognitiveArchitectureBuilder(simName, sim, fsa, neal).build()
 
 narc.addRule(
     
@@ -46,7 +46,7 @@ narc.addRule(
 
 narc.addFact("item", (2,4))
 
-neal.nealApplyProjections()
+narc.apply()
 
 sim.run(50)
 
